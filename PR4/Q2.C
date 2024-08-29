e2283ea4f169ff7cd76c775357309bea903f4898
prince_kakadiya_01 <princekakadiya01@gmail.com>
2024-08-29 12:29:13 +0530

    Add files via upload

---
+++ PR4/Q2.C
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	int i,j,n=11;
+	clrscr();
+	for(i=1;i<=4;i++)
+	{
+	       for(j=1;j<=i;j++)
+	
+	
+	
+	 {
+	   printf("%d",n);
+	   n++;
+	 }
+	   printf("\n");
+	}
+	getch();
+
+}
+
+
+
+
+
+
