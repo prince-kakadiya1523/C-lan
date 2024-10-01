99e8b6795e1011cbc42aa5e68f275eb5552d30a0
prince_kakadiya_01 <princekakadiya01@gmail.com>
2024-10-01 09:47:18 +0530

    Add files via upload

---
+++ exam2/1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+
+    int a,b;
+    
+        printf("Enter any numaber array:");
+        scanf("%d",a);
+    {
+	
+	     printf("Enter any maximun number:");
+	     scanf()
+	
+}
+	
+	
+	
+	
+	
+	}
+    
+    
+    
