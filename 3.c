9caa955a6982e02a3b99603fff5e9e77a25e848a
prince_kakadiya_01 <princekakadiya01@gmail.com>
2024-10-01 09:46:23 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+ main()
+      
+{
+	  
+	   int i,j;
+   
+	   
+	   
+       for(i=j;i>=1;i--)
+       
+	   {
+	   	       for(j=5;j>=1;j--)
+       	
+	   
+	   {
+	   
+	    printf("%d",j);
+	   }
+		   
+		   
+	        printf("\n");
+		   }
+	  
+}
+	   
