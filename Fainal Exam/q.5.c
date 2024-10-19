2e03123ef2e8a2f27abb76231d8814b536f2f09a
prince_kakadiya_01 <princekakadiya01@gmail.com>
2024-10-19 10:41:12 +0530

    Add files via upload

---
+++ Fainal Exam/q.5.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+ mian()
+ 
+ {
+ 	int choice;
+ 	printf("Enter your choice : ");
+ 	scanf("%d",&choice);
+ 	
+ 	switch (choice)
+ 	{
+ 		
+ 	case 1:
+ 	printf("today is monday ");
+ 	break;
+ 
+ 	case 2:
+ 	printf("today is tuesday");
+ 	break;
+ 	
+	case 3:
+ 	printf("today is wedsday " );
+ 	break;
+ 	
+	case 4:
+ 	printf("today is thurs day ");
+ 	break;
+ 	
+	case 5:
+ 	printf("today is friday ");
+ 	break;	
+ 
+ 	case 6:
+	printf("today is satur day ");
+	break;
+	
+	case 7:
+	printf("today is sunday ");	
+	break;
+	
+	default:
+	printf("Enter  unvaild value :" );
+ 		
+
+	 	
+	 }
+ }
